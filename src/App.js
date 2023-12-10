9f1fad28f2ffd6e460d051a3a67cc44716de7bbd
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-12-10 08:30:35 -0500

    app update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import logo from './logo.svg';
+import './App.css';
+
 import React, { useEffect, useState } from "react";
 import {
   Container,
@@ -10,173 +13,6 @@ import {
 } from "react-bootstrap";
 import { FaEthereum } from "react-icons/fa";
 
-// import web3 and contract
-import web3 from "./web3";
-import contract from "./contract";
-// or import all the named exports as an object
-const alchemy = require("alchemy-sdk");
-const alch = alchemy.createAlchemy(process.env.REACT_APP_AlCHEMY_KEY);
-
-// define your state variables
-const [account, setAccount] = useState("");
-const [username, setUsername] = useState("");
-const [nft, setNft] = useState(null);
-const [loading, setLoading] = useState(false);
-
-// define your functions
-const getAccount = async () => {
-  // get the user's MetaMask account
-  const accounts = await web3.eth.getAccounts();
-  if (accounts.length > 0) {
-    setAccount(accounts[0]);
-  } else {
-    alert("Please connect to MetaMask");
-  }
-};
-
-const getFeedback = async (event) => {
-  // prevent the default form submission behavior
-  event.preventDefault();
-  // check if the user has entered a username
-  if (username) {
-    // set the loading state to true
-    setLoading(true);
-    try {
-      // get the NFT id for the username from the smart contract
-      const nftId = await contract.methods
-        .getNftId(username)
-        .call({ from: account });
-      // check if the NFT id is valid
-      if (nftId > 0) {
-        // get the NFT data from the smart contract
-        const nftData = await contract.methods
-          .getNftData(nftId)
-          .call({ from: account });
-        // set the NFT state with the data
-        setNft({
-          id: nftId,
-          title: nftData.title,
-          description: nftData.description,
-          image: nftData.image,
-          fee: nftData.fee,
-        });
-      } else {
-        // create a new NFT for the username using the Chainlink Function
-        const response = await fetch(
-          "https://api.chain.link/v2/functions/<your-function-id>/runs",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "X-Chainlink-EA-AccessKey": "<your-access-key>",
-              "X-Chainlink-EA-Secret": "<your-secret>",
-            },
-            body: JSON.stringify({
-              jobRun: {
-                data: {
-                  username: username,
-                },
-              },
-            }),
-          }
-        );
-        // check if the response is successful
-        if (response.ok) {
-          // get the response data
-          const data = await response.json();
-          // get the NFT id from the data
-          const nftId = data.data.result;
-          // get the NFT data from the smart contract
-          const nftData = await contract.methods
-            .getNftData(nftId)
-            .call({ from: account });
-          // set the NFT state with the data
-          setNft({
-            id: nftId,
-            title: nftData.title,
-            description: nftData.description,
-            image: nftData.image,
-            fee: nftData.fee,
-          });
-        } else {
-          // handle the error
-          alert("Something went wrong");
-        }
-      }
-    } catch (error) {
-      // handle the error
-      console.error(error);
-      alert("Something went wrong");
-    }
-    // set the loading state to false
-    setLoading(false);
-  } else {
-    // handle the empty input
-    alert("Please enter a username");
-  }
-};
-
-//update the NFT feedback using the Chainlink Function
-const updateFeedback = async () => {
-  // check if the user has an NFT
-  if (nft) {
-    // set the loading state to true
-    setLoading(true);
-    try {
-      // update the NFT feedback using the Chainlink Function
-      const response = await fetch(
-        "https://api.chain.link/v2/functions/<your-function-id>/runs",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "X-Chainlink-EA-AccessKey": "<your-access-key>",
-            "X-Chainlink-EA-Secret": "<your-secret>",
-          },
-          body: JSON.stringify({
-            jobRun: {
-              data: {
-                username: username,
-              },
-            },
-          }),
-        }
-      );
-      // check if the response is successful
-      if (response.ok) {
-        // get the response data
-        const data = await response.json();
-        // get the NFT id from the data
-        const nftId = data.data.result;
-        // get the NFT data from the smart contract
-        const nftData = await contract.methods
-          .getNftData(nftId)
-          .call({ from: account });
-        // set the NFT state with the updated data
-        setNft({
-          id: nftId,
-          title: nftData.title,
-          description: nftData.description,
-          image: nftData.image,
-          fee: nftData.fee,
-        });
-      } else {
-        // handle the error
-        alert("Something went wrong");
-      }
-    } catch (error) {
-      // handle the error
-      console.error(error);
-      alert("Something went wrong");
-    }
-    // set the loading state to false
-    setLoading(false);
-  } else {
-    // handle the case when the user does not have an NFT
-    alert("You do not have an NFT to update");
-  }
-};
-
 function App() {
   // define your state variables and functions here
   // ...
@@ -185,13 +21,13 @@ function App() {
     <Container>
       <Row className="justify-content-md-center">
         <Col md="auto">
-          <h1 className="text-center">Avanite</h1>
-          <p className="text-center">
-            A dApp that generates dynamic NFTs based on your Fortnite stats using Avalanche C-Chain, Chainlink oracle and Chainlink functions
+          <h2 className="text-center">Fortnite Stats Feedback</h2>
+          <p class="subtitle" className="text-center">
+            A dApp that generates dynamic NFTs based on your Fortnite stats
           </p>
         </Col>
       </Row>
-      <Row className="justify-content-md-center">
+      <Row class="player-stats" className="justify-content-md-center">
         <Col md="auto">
           <Form>
             <Form.Group className="mb-3" controlId="formUsername">
@@ -204,19 +40,20 @@ function App() {
               />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Get Feedback
+              Get my stats
             </Button>
           </Form>
         </Col>
       </Row>
-      <Row className="justify-content-md-center">
+      <Row class="nft" className="justify-content-md-center">
         <Col md="auto">
           <Card style={{ width: "18rem" }}>
-            <Card.Header>Your Dynamic NFT</Card.Header>
+            <Card.Header class="nft-head">Your Dynamic NFT</Card.Header>
             <Card.Body>
               <Card.Title>{/* display your NFT title here */}</Card.Title>
               <Card.Text>{/* display your NFT description here */}</Card.Text>
               <Image
+                //src={/* display your NFT image here */}
                 alt="NFT image"
                 rounded
               />
--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,16 @@
-import alchemyWeb3 from "@alch/alchemy-web3";
+// import the alchemy-sdk module
+const alchemy = require("alchemy-sdk");
 
-const web3 = alchemyWeb3.createAlchemyWeb3(process.env.REACT_APP_ALCHEMY_KEY);
+// create an alchemy instance with your Alchemy API key
+/*const alch = alchemy.createAlchemy(process.env.REACT_APP_ALCHEMY_KEY);
 
-export default web3;
+// use the alchemy instance to access alchemy-web3 and alchemy-sdk features and methods
+// for example, get the block number using alchemy-web3
+alch.core.getBlockNumber().then((blockNumber) => {
+  console.log(blockNumber);
+});
+
+// for example, get the NFT metadata using alchemy-sdk
+alch.nft.getNftMetadata("0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB", 9775).then((nft) => {
+  console.log(nft);
+});*/
