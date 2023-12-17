9d6350a1d7ac00976233cfa605a20c2fbf17d68d
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-12-17 13:48:01 -0500

    script

--- scripts/05_readResponse.js
+++ scripts/05_readResponse.js
@@ -4,7 +4,7 @@ const { Contract } = require("ethers");
 const { signer } = require("../connection.js");
 const { abi } = require("../contracts/abi/FunctionsConsumer.json");
 
-const consumerAddress = "0x01568F134A64b8c525E468908a3850B6c6A55F54"
+const consumerAddress = "0x78fcba4d007c648e1e4a681e371a0549515753f6"
 const readResponse = async () => {
   const functionsConsumer = new Contract(consumerAddress, abi, signer);
 
---
+++ source.js
@@ -0,0 +1,59 @@
+// Get the username from the request parameters
+const username = args[0];
+
+// Get the API key from the secrets object
+const apiKey = `${secrets.API_KEY}`;
+
+// Check if the username and API key are valid
+if (!username || !apiKey) {
+  throw Error("Username and API key are required");
+}
+
+// Define the API URL and headers
+const url = `https://fortnite-api.com/v1/stats/br/v2?name=${username}`;
+const headers = {
+  "x-api-key": apiKey
+};
+
+// Make the HTTP GET request to the API
+const response = await Functions.makeHttpRequest({
+  url: url,
+  method: "GET",
+  headers: headers
+});
+
+// Check if the response is successful
+if (response.status !== 200) {
+  throw Error(`API request failed: ${response.statusText}`);
+}
+
+// Parse the response data as JSON
+const data = JSON.parse(response.data);
+
+// Check if the data contains the account and stats objects
+if (!data.data || !data.data.account || !data.data.stats) {
+  throw Error("Invalid data format");
+}
+
+// Extract the desired attributes from the data
+const account = data.data.account;
+const stats = data.data.stats;
+
+const id = account.id;
+const user = account.name;
+const image = account.image;
+const score = stats.all.overall.score;
+const kills = stats.all.overall.kills;
+const kd = stats.all.overall.kd;
+const winRate = stats.all.overall.winRate;
+
+// Return the attributes as an object
+return {
+  id: id,
+  name: user,
+  image: image,
+  score: score,
+  kills: kills,
+  kd: kd,
+  winRate: winRate
+};
